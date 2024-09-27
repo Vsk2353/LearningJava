38925e897ce471c9dc0aa8a9a16b51cccebf4c4a
visky <65082238+Vsk2353@users.noreply.github.com>
2024-09-27 22:35:33 +0530

    Implement basic syntax and structure in Basics.java

---
+++ src/Day1/Input.java
@@ -0,0 +1,8 @@
+package Day1;
+import java.util.Scanner; //Shortcut: Scanner
+
+public class Input {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in); //System.in means the input is taken from the keyboard
+    }
+}
---
+++ src/Day1/Output.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class Output{
+    public static void main(String[] args) { //(shortcut: psvm)
+        System.out.print("Hello am "); //(shortcut : sout)
+        System.out.println("Vivek!");
+        System.out.println("Am 21 years old.");
+    }
+}
