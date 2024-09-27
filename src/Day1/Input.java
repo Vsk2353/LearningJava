c91cc04aa26169a7df1308655adb830a8c7f985a
visky <65082238+Vsk2353@users.noreply.github.com>
2024-09-27 22:38:33 +0530

    Implement basic syntax and structure in Basics.java

--- src/Day1/Input.java
+++ src/Day1/Input.java
@@ -4,5 +4,6 @@ import java.util.Scanner; //Shortcut: Scanner
 public class Input {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in); //System.in means the input is taken from the keyboard
+        System.out.println(input.nextLine());
     }
 }
