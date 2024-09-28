52a7cd3127b66b89740b827137f2d0e548ba0ec9
visky <65082238+Vsk2353@users.noreply.github.com>
2024-09-28 08:53:34 +0530

    Add Primitives.java with examples of primitive data types in Java

---
+++ src/Day1/Primitives.java
@@ -0,0 +1,17 @@
+package Day1;
+
+public class Primitives {
+    public static void main(String[] args){
+
+        int rollno=64;
+        long largeIN=48357347534584L; //Uppercase L to be included at the end
+        char letter = 'r';
+        float marks=98.67f; //Lowercase f to be included at the end
+        double largeDN=9375349.467894;
+        boolean check = true;
+
+        //String is not primitive, as it can be broken down into characters.
+        String name = "Vivek";
+
+    }
+}
