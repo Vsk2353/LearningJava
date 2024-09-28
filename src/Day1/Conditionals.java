df456e42845f91fcaf3614817367bb71cfb8c502
visky <65082238+Vsk2353@users.noreply.github.com>
2024-09-28 13:44:34 +0530

    Add conditionals and loop examples

---
+++ src/Day1/Conditionals.java
@@ -0,0 +1,27 @@
+package Day1;
+
+public class Conditionals {
+    public static void main(String[] args){
+
+        //if condition
+
+        int a = 10;
+        if (a == 10) {
+            System.out.println("If condition executed");
+        }
+
+        //while loop
+        int b = 1;
+
+        while(b != 5){
+            System.out.println(b);
+            b++;
+        }
+
+        //for loop
+
+        for(int count = 1; count !=5; count++){
+            System.out.println(count);
+        }
+    }
+}
