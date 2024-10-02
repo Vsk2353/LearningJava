be574c99dab9ba57312d1458e1abe03998ad7650
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-02 13:30:46 +0530

    Add CaseCheck.java to check if input character is uppercase or lowercase

---
+++ src/Day2/CaseCheck.java
@@ -0,0 +1,17 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        char ch = input.next().trim().charAt(0); //trim() removes the extra spaces in front and behind the word
+
+        if (ch>='a' && ch<='z'){
+            System.out.println("Lowercase");
+        }
+        else{
+            System.out.println("Uppercase");
+        }
+    }
+}
