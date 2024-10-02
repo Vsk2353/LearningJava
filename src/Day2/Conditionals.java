f3f0ac828c76ac5c64e492ba22456172dae82919
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-02 13:08:19 +0530

    Add Conditionals.java with if-else examples

---
+++ src/Day2/Conditionals.java
@@ -0,0 +1,16 @@
+package Day2;
+public class Conditionals{
+    public static void main(String[] args){
+
+        int salary = 24500;
+        if (salary > 20000){
+            System.out.println(salary+10000);
+        }
+        else if(salary > 10000){
+            System.out.println(salary+2000);
+        }
+        else{
+            System.out.println(salary);
+        }
+    }
+        }
