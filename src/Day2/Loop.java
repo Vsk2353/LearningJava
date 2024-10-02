f5d856a58b37a220f06b5330dc027959bc217d5c
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-02 13:09:19 +0530

    Add Loop.java with for, while, do while examples

---
+++ src/Day2/Loop.java
@@ -0,0 +1,32 @@
+package Day2;
+import java.util.Scanner;
+public class Loop {
+    public static void main(String[] args){
+        Scanner input  = new Scanner(System.in);
+
+        //for loops
+
+//        int n = input.nextInt();
+//        for(int num=1;num<=n;num+=1){
+//            System.out.println(num);
+
+
+        //while loops
+//        int n = input.nextInt();
+//        int count=1;
+//        while(count <= n){
+//            System.out.println(count);
+//            count+=1;
+
+        //do while loops
+
+        int n = 1;
+        do {
+            System.out.println("Hello");
+        } while(n != 1);
+
+
+
+    }
+}
+
