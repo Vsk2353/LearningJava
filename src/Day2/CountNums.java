b04e737af441b8ca1f829055dfe111873015bf55
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-02 14:03:43 +0530

    Add CountNums.java to find the occurrences of 5 in an integer

---
+++ src/Day2/CountNums.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class CountNums {
+    public static void main(String[] args) {
+        int n = 4553565;
+        int count =0;
+
+        while(n>0){
+
+            if (n%10==5){
+                count+=1;
+            }
+            n=n/10;
+        }
+
+        System.out.println(count);
+    }
+}
