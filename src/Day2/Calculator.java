3bb22752630c8bb944a6e54ab193854a8966bb82
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-02 14:29:17 +0530

    Add Calculator.java to implement basic calculator operations

---
+++ src/Day2/Calculator.java
@@ -0,0 +1,52 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //Take input from user till user does not press X or x
+        while(true){
+            //take the operator as input
+            System.out.println("Enter the operator");
+            char op = in.next().trim().charAt(0);
+            int a;
+            int b;
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%'){
+                System.out.println("Enter the two numbers");
+                a = in.nextInt();
+                b = in.nextInt();
+                if (op == '+'){
+                    System.out.println(a+b);
+                }
+                if (op == '-'){
+                    System.out.println(a-b);
+            }
+                if (op == '*'){
+                    System.out.println(a*b);
+            }
+                if (op == '/'){
+                    if(b==0){
+                        System.out.println("Cannot divide by zero");
+                    }
+                    else{
+                        System.out.println(a / b);
+                    }
+
+            }
+                if (op == '%'){
+                    System.out.println(a%b);
+            }
+            }
+            else if(op == 'x' || op =='X'){
+                System.out.println("Exiting....");
+                break;
+            }
+
+            else{
+                System.out.println("Ivalid operation");
+            }
+        }
+    }
+}
