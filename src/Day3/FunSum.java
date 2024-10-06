a7f6a5058cc27dde132a0694c70de17eadb597bf
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-06 21:28:18 +0530

    Add FunSum.java to calculate sum of two numbers using a function

---
+++ src/Day3/FunSum.java
@@ -0,0 +1,31 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class FunSum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        sum();
+        /*
+        int ans = sum();
+        System.out.println(ans);
+         */
+    }
+
+    static void sum() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int num1 = in.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = in.nextInt();
+        System.out.println("The sum is: " + (num1+num2));
+
+        /*
+        We can instead return the value using: return sum
+         */
+
+        /*
+        So when a value is returned from a function, the function's job is done and the pointer comes out.
+         */
+    }
+}
