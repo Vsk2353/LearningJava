8a211b6e278602bb141a7097b86701d9ef51de0f
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-06 21:29:37 +0530

    Add FunString.java to return a greeting message from a function

---
+++ src/Day3/FunString.java
@@ -0,0 +1,12 @@
+package Day3;
+
+public class FunString {
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+
+    static String greet(){
+        String msg = "Hello! Good morning!";
+        return msg;
+    }
+}
