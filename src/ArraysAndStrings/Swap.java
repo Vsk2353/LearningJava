1a96d5a78901c8dee5026ce9fb460706053af7c0
visky <65082238+Vsk2353@users.noreply.github.com>
2024-10-13 17:13:46 +0530

    Add Swap.java to swap the elements of an array

---
+++ src/ArraysAndStrings/Swap.java
@@ -0,0 +1,18 @@
+package ArraysAndStrings;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+        swap(arr,1,3);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr,int i1,int i2){
+        int temp = arr[i1];
+        arr[i1]=arr[i2];
+        arr[i2]=temp;
+    }
+
+}
